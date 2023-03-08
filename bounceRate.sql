9734681592e9f17c60325dfaf0092bb9637de4a2
Shiyang Yao <104394846+ShawnYao2000@users.noreply.github.com>
2023-03-09 00:57:09 +0800

    Delete bounceRate.sql
    Clean up and switch to maven

--- bounceRate.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-(SELECT CAST(COUNT(exit_date) AS REAL)
-FROM server_log
-WHERE unixepoch(exit_date) - unixepoch(entry_date) < 10 OR exit_date = "n/a")
-/
-(SELECT CAST(COUNT(id) AS REAL)
- FROM click_log);
-
